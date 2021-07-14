733f8db346b4ad096a2f6b0bc0c97395497a502b
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-14 01:31:38 -0300

    feat: modificando ReadOnly dentro del modulo

--- ejemploModulos/modulo_base.js
+++ ejemploModulos/modulo_base.js
@@ -1,5 +1,7 @@
 import nombreDefault, { name, edad } from "./modulo_dos.mjs";
 
+// edad = 50; esta linea tira error edad se importa como constante
+
 console.log(`Hola me llamo ${name} tengo ${edad}`);
 
 // al importar de un modulo default se puede importar y asiganr cualquier nombre
--- ejemploModulos/modulo_dos.mjs
+++ ejemploModulos/modulo_dos.mjs
@@ -8,7 +8,12 @@ Los modulos son cualquier archivo que importe o exporte modulos
 // el nombre de la variable a exporta se conviete
 // el identificador necesario para importar el dato
 export const name = "Fernando";
-export const edad = 28;
+export let edad = 28;
+
+edad = 29;
+// modificando edad luego de exportarlo hace que el valor de
+// en el otro modulo tambien cambie
+// este cambio afecta a todos quienes importaron este modulo
 
 let valorPorDefecto = 20;
 
@@ -19,3 +24,6 @@ export default valorPorDefecto;
 // no se puede definir el valor dentro del export tiene que estar previamente definido
 // ya que el eport defualt es como una asignacion en la que hacemos default = valorPorDefecto
 // si hicieramos la asignacion seria como hacer default = valorPorDefecto = 20
+
+// no se suelen exportar variables definidas con let o var ya que no pueden ser modificadas
+// fuera del modulo es decir son Read Only solo puede cambiar dentro del modulo
