0031c33dd66770c61200e432d34459d5cf7a7300
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-14 00:47:17 -0300

    feat: explicacion inicial de export

---
+++ ejemploModulos/modulo_dos.mjs
@@ -0,0 +1,11 @@
+/*
+Se recomiendo que los archivos que representan modulos
+tegan extension .mjs
+
+Los modulos son cualquier archivo que importe o exporte modulos
+*/
+
+// el nombre de la variable a exporta se conviete
+// el identificador necesario para importar el dato
+export const name = "Fernando";
+
