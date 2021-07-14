1402541aa7bd675d4b2b188526a92b08ca2b73b2
fernandopaz1 <ferpaz9292@gmail.com>
2021-07-14 01:19:22 -0300

    feat: export por default

--- ejemploModulos/modulo_base.js
+++ ejemploModulos/modulo_base.js
@@ -1,3 +1,6 @@
-import { name } from "./modulo_dos.mjs";
+import nombreDefault, { name, edad } from "./modulo_dos.mjs";
 
-console.log(name);
+console.log(`Hola me llamo ${name} tengo ${edad}`);
+
+// al importar de un modulo default se puede importar y asiganr cualquier nombre
+console.log(`El import por default es ${nombreDefault}`);
--- ejemploModulos/modulo_dos.mjs
+++ ejemploModulos/modulo_dos.mjs
@@ -8,4 +8,14 @@ Los modulos son cualquier archivo que importe o exporte modulos
 // el nombre de la variable a exporta se conviete
 // el identificador necesario para importar el dato
 export const name = "Fernando";
+export const edad = 28;
 
+let valorPorDefecto = 20;
+
+export default valorPorDefecto;
+// Este es el valor por defecto que exporta este modulo
+
+// export default let valorPorDefecto = 20;
+// no se puede definir el valor dentro del export tiene que estar previamente definido
+// ya que el eport defualt es como una asignacion en la que hacemos default = valorPorDefecto
+// si hicieramos la asignacion seria como hacer default = valorPorDefecto = 20
